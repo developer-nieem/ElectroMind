dc152f246a307a7ae638b926d5aa44b7e48bdb8a
developer-nieem <naimhossenwork@gmail.com>
2024-05-15 13:07:42 +0600

    setup electro mind project

---
+++ src/layouts/paths.ts
@@ -0,0 +1,18 @@
+export const paths = {
+    root: "/",
+    apps: "/apps",
+    website: {
+      signin: "/signin",
+      signup: "/signup",
+    },
+    product: {
+      root: "/product",
+      category: "/product/category",
+    },
+    cart: {
+      root: "/cart",
+    },
+    page404: "/error/404",
+    checkout: "/checkout",
+  };
+  
